cf240e6e0f259e3a85e5503888f0c04717680ad6
BHAVYA SRI B <bbhavyasri3003@gmail.com>
2026-09-17 14:22:16 +0530

    arithmetic operator

---
+++ DAY01-ARITHMETIC/day 1.py
@@ -0,0 +1,12 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Power:", a ** b)
+print("Modulus:", a % b)
+print("floor division:",a//b)
+print("power:",a**b)
