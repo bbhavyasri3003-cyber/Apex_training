35f69899d5310c95a970e117b7f5c814207da8f2
BHAVYA SRI B <bbhavyasri3003@gmail.com>
2026-09-25 13:38:17 +0530

    day 3

---
+++ day 3/day 3.py
@@ -0,0 +1,9 @@
+Java=float(input("Enter the value:"))
+Python=float(input("Enter the value:"))
+MySQL=float(input("Enter the value:"))
+print("total:",Java+Python+MySQL)
+print("average:",Java+Python+MySQL/3)
+print("percentage:",Java+Python+MySQL/3*100)
+print("remaining marks:",300-Java+Python+MySQL)
+
+      
